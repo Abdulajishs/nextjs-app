86592f8219b1b2fbd66fc898411a2d6fd736d16e
Abdulajishs <abdulajeeshsb@gmail.com>
2024-04-13 10:56:19 +0530

    Final touch to About US page using Link

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -1,9 +1,24 @@
 //our-domain.com/aboutus
 
-const AboutPage = ()=>{
-    return(
-        <h1>Welcome to About-us Page</h1>
-    )
-}
+import Link from "next/link";
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>Welcome to About-us Page</h1>
+      <ul>
+        <li>
+          <Link href="/aboutus/1">Yash</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/2">Vaibhav</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/3">Suresh</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
 
 export default AboutPage;
